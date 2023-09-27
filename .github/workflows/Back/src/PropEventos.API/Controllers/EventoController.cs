e184b59f3b5108d266aebc4909f7cf766e557ceb
CharlesCaetano <Charlescaetano8@gmail.com>
2023-09-27 17:07:53 -0300

    seção 3: introdução ao EF Core

--- .github/workflows/Back/src/PropEventos.API/Controllers/EventoController.cs
+++ .github/workflows/Back/src/PropEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using PropEventos.API.Data;
 using PropEventos.API.Models;
 
 namespace PropEventos.API.Controllers
@@ -12,69 +13,25 @@ namespace PropEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]{
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "ANGULAR 11 e .NET 5",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    ImagemURL = "foto.png"
-                },
-                 new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular suas novidades",
-                    Local = "São Paulo",
-                    Lote = "2º Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                    ImagemURL = "minhafotoEvento2.png"
-                }
-                ,
-                 new Evento(){
-                    EventoId = 3,
-                    Tema = "Angular suas novidades",
-                    Local = "São Paulo",
-                    Lote = "2º Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                    ImagemURL = "minhafotoEvento3.png"
-                },
-                 new Evento(){
-                    EventoId = 4,
-                    Tema = "Angular suas novidades ..",
-                    Local = "São Paulo",
-                    Lote = "2º Lote",
-                    QtdPessoas = 450,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                    ImagemURL = "minhafotoEvento4.png"
-                },
-                 new Evento(){
-                    EventoId = 5,
-                    Tema = "Angular suas novidades ..",
-                    Local = "São Paulo",
-                    Lote = "5º Lote",
-                    QtdPessoas = 450,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                    ImagemURL = "minhafotoEvento5 testessssrr.png"
-                }
-        };
+        
+        private readonly DataContext _context;
 
-         public EventoController()
+         public EventoController(DataContext context)
         {              
+            this._context = context;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetbyId(int id)
+        public Evento GetbyId(int id)
         {    
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(
+                evento => evento.EventoId == id);
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {    
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpPost]
